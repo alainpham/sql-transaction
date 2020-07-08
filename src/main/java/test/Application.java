26bead389dfbabefd358c0d8a98a7742ab4a1f66
Alain Pham <apham@redhat.com>
2020-07-08 15:42:17 +0200

    first commit

---
+++ src/main/java/test/Application.java
@@ -0,0 +1,66 @@
+package test;
+
+import javax.annotation.Resource;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.jms.ConnectionFactory;
+import javax.sql.DataSource;
+import javax.transaction.TransactionManager;
+import javax.transaction.UserTransaction;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.cdi.ContextName;
+import org.apache.camel.cdi.ImportResource;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.component.sql.SqlComponent;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.jta.JtaTransactionManager;
+
+@Named("sql-transactionApp")
+@ImportResource("camel-context.xml")
+public class Application{
+
+    @Inject
+    @ContextName("sql-transaction")
+    CamelContext context;
+
+    @Resource(mappedName = "java:/TransactionManager")
+    private TransactionManager transactionManager;
+    
+    @Resource(mappedName = "java:/JmsConnectionFactory")
+    private ConnectionFactory connectionFactory;
+
+    @Resource 
+    private UserTransaction userTransaction;
+
+    //change this to be able to use database connections
+    @Resource(mappedName = "java:jboss/datasources/postgresqlds") 
+    private DataSource dataSource;
+
+    @Produces
+    @Named("sql")
+    public SqlComponent createSqlComponent(){
+        SqlComponent sql= new SqlComponent(context);
+        sql.setDataSource(dataSource);
+        return sql;
+    }
+
+    @Produces
+    @Named("jms")
+    public JmsComponent createJmsComponent(PlatformTransactionManager transactionManager) {
+        return JmsComponent.jmsComponentTransacted(connectionFactory, transactionManager);
+    }
+
+    @Produces
+    @Named("transactionManager")
+    public PlatformTransactionManager createTransactionManager() {
+        JtaTransactionManager jtaTransactionManager = new JtaTransactionManager();
+        jtaTransactionManager.setUserTransaction(userTransaction);
+        jtaTransactionManager.setTransactionManager(transactionManager);
+        jtaTransactionManager.afterPropertiesSet();
+        return jtaTransactionManager;
+    }
+
+
+}
---
+++ src/main/resources/db.sql
@@ -0,0 +1,4 @@
+CREATE TABLE account(
+	user_id serial PRIMARY KEY,
+	email VARCHAR (355) UNIQUE NOT NULL
+);
